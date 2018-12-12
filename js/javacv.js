3a5ae8671b62621b2d9f3e450aff0d653f73c2a4
simonot <cyrilsimonot@yahoo.fr>
2018-12-12 10:39:21 +0100

    monCv

---
+++ js/javacv.js
@@ -0,0 +1,6 @@
+function github(){
+    document.location.href='https://github.com/cyrheal';
+};
+function linkedIn(){
+    document.location.href='https://www.linkedin.com/in/cyril-simonot-807ab516b/';
+};
